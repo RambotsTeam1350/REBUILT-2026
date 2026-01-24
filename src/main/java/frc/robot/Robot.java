67be83de9d59feba2d100582b74f7522aa2e77df
bellacaporasoxX <icaporaso26@lasalle-academy.org>
2026-01-24 15:55:21 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -72,4 +73,10 @@ public class Robot extends TimedRobot {
 
   @Override
   public void simulationPeriodic() {}
+
+  @Override
+  public void robotInit() {
+  CameraServer.startAutomaticCapture();
+  }
 }
+
