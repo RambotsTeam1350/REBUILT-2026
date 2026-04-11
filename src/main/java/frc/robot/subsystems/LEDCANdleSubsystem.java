7eb82d3be2ab9a4ef9a32d63c021f4b1cfb41160
Joe Alba <joe@joealba.com>
2026-04-11 11:05:02 -0400

    Convert CANdle code from old phoenix to phoenix6

--- src/main/java/frc/robot/subsystems/LEDCANdleSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDCANdleSubsystem.java
@@ -1,57 +1,57 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.led.CANdle;
-import com.ctre.phoenix.led.CANdle.LEDStripType;
-import com.ctre.phoenix.led.CANdle.VBatOutputMode;
-import com.ctre.phoenix.led.CANdleConfiguration;
-import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
-import com.ctre.phoenix.led.LarsonAnimation.BounceMode;
-import com.ctre.phoenix.led.TwinkleAnimation.TwinklePercent;
-import com.ctre.phoenix.led.TwinkleOffAnimation.TwinkleOffPercent;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.configs.CANdleConfiguration;
+import com.ctre.phoenix6.controls.SolidColor;
+import com.ctre.phoenix6.signals.LossOfSignalBehaviorValue;
+import com.ctre.phoenix6.signals.RGBWColor;
+import com.ctre.phoenix6.signals.StatusLedWhenActiveValue;
+import com.ctre.phoenix6.signals.StripTypeValue;
+import com.ctre.phoenix6.signals.VBatOutputModeValue;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDCANdleSubsystem extends SubsystemBase {
-    
-    private final CANdle CANdle = new CANdle(35);
-    private final CANdleConfiguration config = new CANdleConfiguration();
+
+    private final CANdle candle = new CANdle(35);
+
+    private final SolidColor redControl = new SolidColor(0, 511).withColor(new RGBWColor(255, 0, 0));
+    private final SolidColor greenControl = new SolidColor(0, 511).withColor(new RGBWColor(0, 255, 0));
+    private final SolidColor whiteControl = new SolidColor(0, 511).withColor(new RGBWColor(255, 255, 255));
+    private final SolidColor offControl = new SolidColor(0, 511).withColor(new RGBWColor(0, 0, 0));
 
     public LEDCANdleSubsystem() {
+        CANdleConfiguration config = new CANdleConfiguration();
 
-        this.config.statusLedOffWhenActive = false;
-        this.config.disableWhenLOS = false;
-        this.config.stripType = LEDStripType.RGB;
-        this.config.brightnessScalar = 1;
-        this.config.vBatOutputMode = VBatOutputMode.Modulated;
+        config.LED.StripType = StripTypeValue.RGB;
+        config.LED.BrightnessScalar = 1.0;
+        config.LED.LossOfSignalBehavior = LossOfSignalBehaviorValue.KeepRunning;
 
-        this.CANdle.configAllSettings(this.config, 100);
+        config.CANdleFeatures.StatusLedWhenActive = StatusLedWhenActiveValue.Enabled;
+        config.CANdleFeatures.VBatOutputMode = VBatOutputModeValue.Modulated;
 
+        candle.getConfigurator().apply(config);
     }
+
     // If CANdle is Red, the bot is not in range to shoot, so the bot needs to move to the desired location
     public Command LEDRed() {
-        return Commands.runOnce(() -> {
-            this.CANdle.setLEDs(255, 0, 0);
-        });
+        return Commands.runOnce(() -> candle.setControl(redControl));
     }
+
     // If CANdle is Green, bot is in range to both align and shoot
     public Command LEDGreen() {
-         return Commands.runOnce(() -> {
-            this.CANdle.setLEDs(0, 255, 0);
-        });
+        return Commands.runOnce(() -> candle.setControl(greenControl));
     }
+
     // If CANdle is White
-        public Command LEDWhite() {
-         return Commands.runOnce(() -> {
-            this.CANdle.setLEDs(255, 255, 255);
-        });
+    public Command LEDWhite() {
+        return Commands.runOnce(() -> candle.setControl(whiteControl));
     }
+
     // If CANdle is Off, the bot is either off or dead
     public Command LEDOff() {
-        return Commands.runOnce(() -> {
-            this.CANdle.setLEDs(0, 0, 0);
-        });
+        return Commands.runOnce(() -> candle.setControl(offControl));
     }
- }
-
+}
